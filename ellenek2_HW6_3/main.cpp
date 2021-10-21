494cfcbc32e965ad8321d0f38e0731c04da01e9d
ellenkirsh <ellenek2@illinois.edu>
2021-10-20 19:37:28 -0500

    Initial Commit

---
+++ ellenek2_HW6_3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ellenek2_HW6_3
+//
+//  Created by Ellen Kirsh on 10/20/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
